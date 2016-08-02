a5c532a3d7b6b335a47e851e37a1ab7148f62ff0
Sia <siakaramalegos@gmail.com>
2016-08-02 17:39:04 -0500

    Red tests for title

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,19 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "Mardi Paws"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | #{@base_title}"
   end
 end
